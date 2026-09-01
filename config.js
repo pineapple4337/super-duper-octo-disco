008c72009ce604d2d4f1d5e80ce0fa029d0fb9ac
pineapple4337 <ateghddw@gmail.com>
2026-09-01 14:46:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-export const firebaseConfig = {
-  apiKey: "PASTE_FROM_FIREBASE",
-  authDomain: "PASTE_FROM_FIREBASE",
-  projectId: "PASTE_FROM_FIREBASE",
-  storageBucket: "PASTE_FROM_FIREBASE",
-  messagingSenderId: "PASTE_FROM_FIREBASE",
-  appId: "PASTE_FROM_FIREBASE"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAluB3TIq2mrOIr_RLqEkalPVfQi-c69jg",
+  authDomain: "lemon-66f65.firebaseapp.com",
+  projectId: "lemon-66f65",
+  storageBucket: "lemon-66f65.firebasestorage.app",
+  messagingSenderId: "558334181981",
+  appId: "1:558334181981:web:c2a1edaa0c981d937cc3b4",
+  measurementId: "G-5L499F4V3X"
 };
